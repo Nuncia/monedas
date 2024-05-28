56dc0050c12b32c466b23f74f7912ecd7b5e4b18
Monica <maaylahuerta@gmail.com>
2024-05-28 17:56:50 -0400

    Se genera deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,59 @@
 import MyApp from './components/MyApp';
 // import Buscador from './components/Buscador';
-import './App.css'
+import './App.css';
 import { useState } from 'react';
 import Buscador from './components/Buscador';
 
 function App() {
-  const [busqueda, setBusqueda] = useState('')  
+   const [busqueda, setBusqueda] = useState('');
 
-  return (
-    <div className='contenedor'>
-      <header>
-        <h2 style={{textAlign: 'center'}}>Indicadores económicos</h2>
-      </header>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{width: '-webkit-fill-available'}}>
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Home</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+   return (
+      <div className="contenedor">
+         <header>
+            <h2 style={{ textAlign: 'center' }}>Indicadores económicos</h2>
+         </header>
+         <nav
+            className="navbar navbar-expand-lg navbar-dark bg-dark"
+            style={{ width: '-webkit-fill-available' }}
+         >
+            <div className="container-fluid">
+               <a className="navbar-brand" href="#">
+                  Home
+               </a>
+               <button
+                  className="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+               >
+                  <span className="navbar-toggler-icon"></span>
+               </button>
+               <div
+                  className="collapse navbar-collapse"
+                  id="navbarSupportedContent"
+               >
+                  {/* <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">Graficos</a>
               </li>
-            </ul>
-            <Buscador busqueda={busqueda} setBusqueda={setBusqueda}/>
-          </div>
-        </div>
-      </nav>
-      <section>
-        <div className='appComponente'>
-          <MyApp busqueda={busqueda}/>
-        </div>
-      </section>
-      <footer className='footer'>
-         <p>Todos los derechos reservados. Mónica Ayala</p>    
-      </footer>
-    </div>
-  )
+            </ul> */}
+                  <Buscador busqueda={busqueda} setBusqueda={setBusqueda} />
+               </div>
+            </div>
+         </nav>
+         <section>
+            <div className="appComponente">
+               <MyApp busqueda={busqueda} />
+            </div>
+         </section>
+         <footer className="footer">
+            <p>Todos los derechos reservados. Mónica Ayala</p>
+         </footer>
+      </div>
+   );
 }
 
-export default App
+export default App;
